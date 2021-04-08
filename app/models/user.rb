92222c1e2c76a340c4e9ac0a0feae2fc328a4ec4
Marnoel de Guzman <marnoeldeguzman@gmail.com>
2021-04-08 23:50:51 +0800

    add_user_id_to_tasks

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :categories, :dependent => :destroy
-  has_many :tasks, through: :categories
+  has_many :tasks
 validates :username, :email, presence: true
 validates :username, :email, uniqueness: true
 validates :password, presence: true
