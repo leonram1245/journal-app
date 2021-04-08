748d1aa6f928883a140e1ce7527a2faf3d78ce2e
Marnoel de Guzman <marnoeldeguzman@gmail.com>
2021-04-09 01:51:25 +0800

    added_breacrumbs_and_fixed_ui_for_categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_08_154923) do
+ActiveRecord::Schema.define(version: 2021_04_08_155109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,4 +51,5 @@ ActiveRecord::Schema.define(version: 2021_04_08_154923) do
 
   add_foreign_key "categories", "users"
   add_foreign_key "tasks", "categories"
+  add_foreign_key "tasks", "users"
 end
