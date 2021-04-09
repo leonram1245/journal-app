f86e87ac78d2e87b742bbd40f1215ac13a9d4462
Marnoel de Guzman <marnoeldeguzman@gmail.com>
2021-04-10 06:33:21 +0800

    remove_task_helper

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,16 +1,3 @@
 module TasksHelper
-    def errors_for(task)
-        if task.errors.any?
-            content_tag(:div, class: "card border-danger") do
-                concat(content_tag(:div, class: "card-header bg-danger text-white") do
-                    concat "#{pluralize(task.errors.count, "error")} prohibited this #{task.class.name.downcase} from being saved:"
-                end)
-                concat(content_tag(:ul, class: 'mb-0 list-group list-group-flush') do
-                    task.errors.full_messages.each do |msg|
-                        concat content_tag(:li, msg, class: 'list-group-item')
-                    end
-                end)
-            end
-        end
-    end
+  
 end
